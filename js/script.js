8e51c165a0a4ee171f31ff33f1cbfb5f037a5302
anjie_f <amfarmer2022@gmail.com>
2024-03-08 13:08:42 -0600

    Added mouseover stats

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var fans = document.querySelector(".fans");
+fans.addEventListener("click", function() {
+    fans.innerText = "123K";
+});
+var fish = document.querySelector(".fish");
+fish.addEventListener("click", function() {
+    fish.innerText = "47K";
+});
+var pets = document.querySelector(".pets");
+pets.addEventListener("click", function() {
+    pets.innerText = "20K";
+});
